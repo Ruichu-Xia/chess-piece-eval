53204717ce819ad085462c5b057794c15cb8fba9
Eric Xia <ericxia@EricXia-MBP.lan>
2025-01-08 16:18:47 -0800

    Improved model

--- src/model/model.py
+++ src/model/model.py
@@ -3,18 +3,25 @@ import torch.nn.functional as F
 
 
 class DynamicEvalModel(nn.Module):
-    def __init__(self, num_squares, other_features):
+    def __init__(self, num_squares, other_features, num_hidden_layers=2):
         super(DynamicEvalModel, self).__init__()
         self.input_dim = num_squares + other_features
         self.hidden_dim = 128
+        self.num_hidden_layers = num_hidden_layers
 
-        self.fc1 = nn.Linear(self.input_dim, self.hidden_dim)
-        self.fc2 = nn.Linear(self.hidden_dim, self.hidden_dim)
-        self.fc3 = nn.Linear(self.hidden_dim, num_squares)
+        self.input_layer = nn.Linear(self.input_dim, self.hidden_dim)
+        self.hidden_layers = nn.ModuleList(
+            [nn.Linear(self.hidden_dim, self.hidden_dim)
+             for _ in range(self.num_hidden_layers)]
+        )
+        self.output_layer = nn.Linear(self.hidden_dim, num_squares)
 
     def forward(self, x):
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.leaky_relu(self.input_layer(x))
+
+        for layer in self.hidden_layers:
+            x = F.leaky_relu(layer(x))
+
+        x = self.output_layer(x)
 
         return x
